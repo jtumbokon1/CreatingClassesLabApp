2a5db71c07c63a29cc5269a19e66632180a59c9f
Jirch Tumbokon <jirchgot@gmail.com>
2025-01-23 08:54:50 -0700

    Created Person and Relation

--- CreatingClassesLab/Person.cs
+++ CreatingClassesLab/Person.cs
@@ -8,6 +8,53 @@ namespace CreatingClassesLab
 {
     public class Person
     {
+        // private data
+        private int personId;
+        private int age;
 
+        // public properties
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FavoriteColour { get; set; }
+        public int Age
+        {
+            get { return age; }
+            set { age = Math.Abs(value); }
+        }
+        public bool IsWorking { get; set; }
+        // constructor
+        public Person(string firstName, string lastName, string favoriteColour, int age, bool isWorking)
+        {
+            PersonId = personId;
+            personId++;
+            FirstName = firstName;
+            LastName = lastName;
+            FavoriteColour = favoriteColour;
+            Age = age;
+            IsWorking = isWorking;
+        }
+        // public methods
+        public string DisplayPersonInfo()
+        {
+            return $"{personId}: {FirstName} {LastName}'s favorite coloyur is {FavoriteColour}";
+        }
+        public string ChangeFavoriteColour()
+        {
+            return FavoriteColour = "White";
+        }
+        public int GetAgeInTenYears()
+        {
+            return Age + 10;
+        }
+        public override string ToString()
+        {
+            return $"PersonId: {PersonId}\n" +
+                $"FirstName: {FirstName}\n" +
+                $"LastName: {LastName}\n" +
+                $"FavoriteColour: {FavoriteColour}\n" +
+                $"Age: {Age}\n" +
+                $"IsWorking: {IsWorking}";
+        }
     }
 }
--- CreatingClassesLab/Relation.cs
+++ CreatingClassesLab/Relation.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace CreatingClassesLab
 {
+    // enumeration type for possible relationship
+    public enum RelationshipType { Sister, Brother, Mother, Father }
     public class Relation
     {
-    }
-}
+        // private data
+        private RelationshipType r;
+        // public properties
+        public RelationshipType R { get; set; }
+        // constructor
+        public Relation(RelationshipType r)
+        {
+            R = r;
+        }
+        // public methods
+        public string ShowRelationship(Person person1, Person person2)
+        {
+            return $"Relationship between {person1.FirstName} and {person2.FirstName} is: {r}hood";
+        }
+    } // class
+} // namespace
